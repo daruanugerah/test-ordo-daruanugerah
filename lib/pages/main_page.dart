b9266a0642cc97ab0f212216499559a0b9755725
daruanugerah <anugerahdaru@gmail.com>
2021-08-26 19:36:52 +0700

    slicing main page

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:test_ordo/theme.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfffefefe),
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 179,
+          left: 50,
+          right: 50,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Made by',
+                style: textStyle.copyWith(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Daru Anugerah Setiawan',
+              style: textStyle.copyWith(
+                fontSize: 20,
+                fontWeight: semiBold,
+              ),
+            ),
+            SizedBox(
+              height: 107,
+            ),
+            Container(
+              height: 58,
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(66),
+                  ),
+                ),
+                child: Text(
+                  'Test 1',
+                  style: textStyle.copyWith(
+                    color: Colors.white,
+                    fontWeight: semiBold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 26,
+            ),
+            Container(
+              height: 58,
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(66),
+                  ),
+                ),
+                child: Text(
+                  'Test 2',
+                  style: textStyle.copyWith(
+                    color: Colors.white,
+                    fontWeight: semiBold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 26,
+            ),
+            Container(
+              height: 58,
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(66),
+                  ),
+                ),
+                child: Text(
+                  'Test 3',
+                  style: textStyle.copyWith(
+                    color: Colors.white,
+                    fontWeight: semiBold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
