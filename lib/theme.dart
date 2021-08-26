b273c432507517416921e7e8a890949415ef4793
daruanugerah <anugerahdaru@gmail.com>
2021-08-26 19:36:33 +0700

    add theme color and textstyle

---
+++ lib/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color backgroundColor = Color(0xffFCF8F8);
+
+Color orangeColor = Color(0xffFF9A00);
+Color greenColor31 = Color(0xff41BE06);
+Color greenColor32 = Color(0xff3D6670);
+Color disableColor3 = Color(0xff94AFB6);
+Color blueColor3 = Color(0xff53B2FC);
+Color greyColor3 = Color(0xff6C757D);
+Color blackColor3 = Color(0xff212529);
+
+Color blackColor21 = Color(0xff202237);
+Color blackColor22 = Color(0xff262626);
+
+Color blackColor11 = Color(0xff061737);
+Color greenColor11 = Color(0xff47623F);
+Color greenColor12 = Color(0xff92D274);
+Color disableColor1 = Color(0xff979696);
+Color textColor1 = Color(0xff8A7F7F);
+
+TextStyle textStyle = GoogleFonts.poppins();
+
+FontWeight light = FontWeight.w300;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w700;
+FontWeight bold = FontWeight.bold;
